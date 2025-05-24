17bbac64ac4cb09c21336bb34bee34a796ea33e4
Tturna <42873350+Tturna@users.noreply.github.com>
2025-05-24 15:20:24 +0300

    traefik version

--- start-instance.sh
+++ start-instance.sh
@@ -5,4 +5,9 @@ APP_NAME=$1
 docker run --rm -d --network path-deployments-net \
     --name $APP_NAME \
     -e DJANGO_APP_NAME=$APP_NAME \
+    --label traefik.enable=true \
+    --label traefik.http.routers.$APP_NAME-router.rule="PathPrefix(\"/$APP_NAME\")" \
+    --label traefik.http.middlewares.$APP_NAME-strip.stripprefix.prefixes=/$APP_NAME \
+    --label traefik.http.routers.$APP_NAME-router.middlewares=$APP_NAME-strip@docker \
+    --label traefik.http.services.$APP_NAME-service.loadbalancer.server.port=8000 \
     path-deployments-app
