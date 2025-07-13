635b54662f91e1cec5f4df2d2897aaf1047ee012
Tturna <42873350+Tturna@users.noreply.github.com>
2025-07-13 12:30:42 +0300

    register location models to admin

--- dashboard/main/admin.py
+++ dashboard/main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import AppInstanceModel, OrganizationEntity, AppPresetModel, TemplateFileModel, AppConnectionModel
+from .models import AppInstanceModel, OrganizationEntity, AppPresetModel, TemplateFileModel, AppConnectionModel, LocationModel
 
 admin.site.register(AppInstanceModel)
 admin.site.register(OrganizationEntity)
+admin.site.register(LocationModel)
 admin.site.register(AppPresetModel)
 admin.site.register(TemplateFileModel)
 admin.site.register(AppConnectionModel)
